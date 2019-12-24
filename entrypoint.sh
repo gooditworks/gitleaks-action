3d12bf081c16a9b5d6e396c83ca6d4616e58d297
Sergey Petrov <sergey.petrov@me.com>
2019-12-24 15:21:28 +0700

    Добавил переход в рабочую директорию при gitleaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e # Abort script at first error
 set -u # nounset - Attempt to use undefined variable outputs error message, and forces an exit
 # set -x # verbose (expands commands)
 
-gitleaks -v --redact --threads=1 \
-  --repo-path=$GITHUB_WORKSPACE
+cd $GITHUB_WORKSPACE
+gitleaks -v --redact --threads=1
 
